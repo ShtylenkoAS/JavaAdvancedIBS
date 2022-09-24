fcf978986c1ebe6b1341bf296cb007d1f9763afd
ShtylenkoAS <73440850+ShtylenkoAS@users.noreply.github.com>
2022-09-24 11:01:24 +0300

    Task03 Поправил опеределение максимального отрицательного и минимального пложительного элемента

--- src/main/java/Task03Arrays.java
+++ src/main/java/Task03Arrays.java
@@ -18,33 +18,42 @@ public class Task03Arrays {
         System.out.println("Generated array: ");
         System.out.println(Arrays.toString(array));
 
-        int[] clone_array = array.clone();
 
-        Arrays.sort(array);
-        System.out.println("Sorted array: ");
-        System.out.println(Arrays.toString(array));
+//        Делал сортировку и клонированный массив т.к. работать с отсортированным массивом удобнее и более наглядно
+//        Учитывая следюущее задание по поиску элементов было бы весьма удобно пользоваться именно отсортированным clone_array, т.к. можно было бы взять индекс справа и слева от 0
+//        int[] clone_array = array.clone();
+//
+//        Arrays.sort(array);
+//        System.out.println("Sorted array: ");
+//        System.out.println(Arrays.toString(array));
 
-        int min_negative = array[0];
-        int min_positive = right;
-        for (int j : array) {
-            if (j >= 0) {
-                min_positive = j;
-                break;
-            }
-        }
-
-        for (int i = 0; i < clone_array.length; i++) {
-            if (clone_array[i] == min_negative) {
-                clone_array[i] = min_positive;
-            }else if (clone_array[i] == min_positive) {
-                clone_array[i] = min_negative;
+        int max_negative = -11;
+        int min_positive = 11;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] < 0 && array[j] > max_negative) {
+                max_negative = array[j];
+            } else if (array[j] > 0 && array[j] < min_positive) {
+                min_positive = array[j];
             }
         }
 
         System.out.println("Minimum positive value in the array: " + min_positive);
-        System.out.println("Minimum negative value in the array: " + min_negative);
+        System.out.println("Maximum negative value in the array: " + max_negative);
 
         System.out.println("The generated array, but the minimum negative value and the minimum positive value are changed: ");
-        System.out.println(Arrays.toString(clone_array));
+        System.out.print("[");
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == max_negative) {
+                array[i] = min_positive;
+            } else if (array[i] == min_positive) {
+                array[i] = max_negative;
+            }
+            if (i < array.length - 1) {
+                System.out.print(array[i] + ", ");
+            } else {
+                System.out.print(array[i]);
+            }
+        }
+        System.out.print("]");
     }
 }
